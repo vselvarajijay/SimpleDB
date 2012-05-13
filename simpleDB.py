97f1ba3e8bc4e6e1a8da46f93d65d5eddc5db85a
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2012-05-13 15:31:06 -0400

    Adding sample data and modified simpleDB.py to work as a Object

--- simpleDB.py
+++ simpleDB.py
@@ -1,90 +1,92 @@
 import fileinput
 import operator
 
+class SimpleDB:
 
-commit_transaction_level = 0
-current_transaction_level = 0
+    def __init__(self):
+        self.dict = {}
+        self.commit_transaction_level = 0
+        self.current_transaction_level = 0
 
+    def set(self, name, value):
+        if name in self.dict:
+            self.dict[name][self.current_transaction_level] = value
+        else:
+            self.dict[name] = {self.current_transaction_level:value}
 
-dict = {}
+    def unset(self, name):
+	    if name in self.dict:
+		    self.dict[name][self.current_transaction_level] = 'NULL'
 
-def set(name, value):
-	if name in dict:
-		dict[name][current_transaction_level] = value
-	else:
-		dict[name] = {current_transaction_level:value}
+    def get(self, name):
+        if name in self.dict and len(self.dict[name]) > 0:
+            print max(self.dict[name].iteritems(), key=operator.itemgetter(0))[1]
+        else:
+            print 'NULL'
 
-def unset(name):
-	if name in dict:
-		dict[name][current_transaction_level] = 'NULL'
+    def rollback(self):
+        if self.current_transaction_level == self.commit_transaction_level:
+            print 'INVALID ROLLBACK'
+            return
 
-def get(name):
+        self.current_transaction_level = self.current_transaction_level - 1
 
-	if name in dict and len(dict[name]) > 0:
-		print max(dict[name].iteritems(), key=operator.itemgetter(0))[1]
-	else:
-		print 'NULL'
+        for name in self.dict:
+            del_list = []
+            for transaction in self.dict[name]:
+                if transaction > self.current_transaction_level:
+                    del_list.append(transaction)
 
-def rollback():
-	global current_transaction_level
-	if current_transaction_level == commit_transaction_level:
-		print 'INVALID ROLLBACK'
-		return
+            for transaction in del_list:
+                    del self.dict[name][transaction]
 
-	current_transaction_level = current_transaction_level - 1
-	for name in dict:
-		del_list = []
-		for transaction in dict[name]:
-			if transaction > current_transaction_level:
-				del_list.append(transaction)
+    def begin(self):
+        self.current_transaction_level = self.current_transaction_level + 1
 
-		for transaction in del_list:
-			del dict[name][transaction]
-
-def commit():
-	global commit_transaction_level
-	commit_transaction_level = current_transaction_level
+    def commit(self):
+        self.commit_transaction_level = self.current_transaction_level
 	
-def equalto(value):
-	equal_list = []
-	for name in dict:
-		dict_val = max(dict[name].iteritems(), key=operator.itemgetter(0))[1]
-		if value==dict_val:
-			equal_list.append(name)
+    def equalto(self, value):
+        equal_list = []
+        for name in self.dict:
+            dict_val = max(self.dict[name].iteritems(), key=operator.itemgetter(0))[1]
+            if value==dict_val:
+                equal_list.append(name)
 
-	if len(equal_list) == 0:
-		print 'NONE'
-	else:
-		print " ".join(equal_list)
+        if len(equal_list) == 0:
+            print 'NONE'
+        else:
+            print " ".join(equal_list)
+
+
 
 
 def main(input_cmd):
-	global current_transaction_level
+    db = SimpleDB()
 
-	for input in input_cmd:
-		input = input.rstrip('\n')
-		input = input.split(' ')
+    for input in input_cmd:
+        input = input.rstrip('\n')
+        input = input.split(' ')
 
-		command = input[0].lower()
+        command = input[0].lower()
 
-		if command == 'set':
-			set(input[1], input[2])
-		elif command == 'unset':
-			unset(input[1])
-		elif command == 'get':
-			get(input[1])
-		elif command == 'equalto':
-			equalto(input[1])
-		elif command == 'begin':
-			current_transaction_level = current_transaction_level + 1
-		elif command == 'commit':
-			commit()
-		elif command == 'rollback':
-			rollback()
-		elif command == 'end':
-			return
-	
+        if command == 'set':
+            db.set(input[1], input[2])
+        elif command == 'unset':
+            db.unset(input[1])
+        elif command == 'get':
+            db.get(input[1])
+        elif command == 'equalto':
+            db.equalto(input[1])
+        elif command == 'begin':
+            db.begin()
+        elif command == 'commit':
+            db.commit()
+        elif command == 'rollback':
+            db.rollback()
+        elif command == 'end':
+            return
 
 if __name__=="__main__":
-	main(fileinput.input())	
+    main(fileinput.input())
 
